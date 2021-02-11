29be1ee1c02a1f4c2dac4626959d286242a04dc7
unknown <andrew.zavatin@yahoo.com>
2021-02-11 21:38:32 +0200

    start

---
+++ precache-manifest.51db56d6d7cde3c0e54fc400d0cdf0ab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ffaff5186dc65e2fbe75105a7cab99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e14e873d9e65c2d1877",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "e5299d42351c7f138860",
+    "url": "/static/css/main.14cc8e58.chunk.css"
+  },
+  {
+    "revision": "1e14e873d9e65c2d1877",
+    "url": "/static/js/2.5a299f19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a299f19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5299d42351c7f138860",
+    "url": "/static/js/main.d7b542a0.chunk.js"
+  },
+  {
+    "revision": "be723b4cb377c85ba67c",
+    "url": "/static/js/runtime-main.7015206d.js"
+  },
+  {
+    "revision": "4142aaeecd6c7e7d84279e443cb6ed3c",
+    "url": "/static/media/MOB-login-versiunea.4142aaee.png"
+  },
+  {
+    "revision": "f25915b34e4cc61a9ade9982c12f54fa",
+    "url": "/static/media/arrow.f25915b3.PNG"
+  },
+  {
+    "revision": "e8a10378d845ef4f2661bb439ecd6acb",
+    "url": "/static/media/inf.e8a10378.PNG"
+  }
+]);
